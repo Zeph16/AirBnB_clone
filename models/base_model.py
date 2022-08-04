bf5dea8c3f8585e5dfdba5a1dae0a48b0c16dd78
Sofonias Ayele <sof16ayele@gmail.com>
2022-08-05 02:01:41 +0300

    Minor fixes, added unittests for BaseModel and FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
                 if k == 'created_at' or k == 'updated_at':
                     v = datetime.strptime(kwargs[k], "%Y-%m-%dT%H:%M:%S.%f")
                 if k != '__class__':
-                    setattr(self, key, value)
+                    setattr(self, k, v)
             return
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
@@ -37,7 +37,7 @@ class BaseModel:
         """ Returns the dictionary representation of the class """
         new_dict = {}
 
-        for key, value in self.__dict__.items():
+        for key, values in self.__dict__.items():
             if key == "created_at" or key == "updated_at":
                 new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
             else:
